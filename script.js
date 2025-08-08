7e455bbc5adc393d0b09428a45926cfbc494cba8
Bordigon <gistavovg@gmail.com>
2025-08-08 16:31:28 -0400

    segundo commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+/* parte del boton tomada de la documentacion de bootstrap que no se si vaya adaptar a la final
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+} */
